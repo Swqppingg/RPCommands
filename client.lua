2d36cd0a1e63028314679eb5a649965a74bb1da8
Swqpping <58861408+Swqppingg@users.noreply.github.com>
2021-03-05 08:28:50 +0100

    Added chat suggestions

--- client.lua
+++ client.lua
@@ -9,3 +9,15 @@ AddEventHandler('sendMessageShowID', function(id, name1, name2)
     TriggerEvent('chatMessage', "^2ID ^0|", {0, 150, 200}, " ^2First Name: ^0" .. name1 .." ^0| ^2Last Name: ^0".. name2 .. " ")
   end
 end)
+----------------------------------------------------------------------------
+
+	Citizen.CreateThread(function()
+			TriggerEvent('chat:addSuggestion', '/twt', 'Tweet something')
+      TriggerEvent('chat:addSuggestion', '/dispatch', 'Dispatch')
+			TriggerEvent('chat:addSuggestion', '/darkweb', 'Send a message on the darkweb')
+      TriggerEvent('chat:addSuggestion', '/news', 'News')
+			TriggerEvent('chat:addSuggestion', '/do', 'Describe an action you are doing.')
+      TriggerEvent('chat:addSuggestion', '/ooc', 'Out of Character chat')
+			TriggerEvent('chat:addSuggestion', '/me', 'Player action')
+      TriggerEvent('chat:addSuggestion', '/showid', 'Show your first name and last name')
+	end)
